038b434f6e1011fa56dbbe120977aee1bce3af25
Andrey Dyldin <and@cesbo.com>
2022-12-03 12:45:23 +0200

    helpscout: article

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -9,12 +9,56 @@ import (
 )
 
 func main() {
-	collections, err := helpscout.ListCollections()
+	var (
+		err  error
+		data []byte
+	)
+
+	fmt.Println("List collections")
+
+	var collections *helpscout.Collections
+	collections, err = helpscout.ListCollections()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data, err := json.Marshal(collections)
+	data, err = json.Marshal(collections)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(data))
+
+	if len(collections.Items) == 0 {
+		return
+	}
+
+	fmt.Println("List categories in", collections.Items[0].Name)
+
+	// get categories from first collection
+	var categories *helpscout.Categories
+	categories, err = helpscout.ListCategories(collections.Items[0].Id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err = json.Marshal(categories)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(data))
+
+	fmt.Println("List articles in", categories.Items[0].Name)
+
+	// get articles from first category
+	var articles *helpscout.Articles
+	articles, err = helpscout.ListArticles(categories.Items[0].Id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err = json.Marshal(articles)
 	if err != nil {
 		log.Fatal(err)
 	}
---
+++ pkg/helpscout/article.go
@@ -0,0 +1,50 @@
+package helpscout
+
+import "time"
+
+// Description: https://developer.helpscout.com/docs-api/objects/article/
+// Endpoint: https://docsapi.helpscout.net/v1/articles/{id}
+type Article struct {
+	Id           string    `json:"id"`
+	Number       int       `json:"number"`
+	CollectionId string    `json:"collectionId"`
+	Slug         string    `json:"slug"`
+	Status       string    `json:"status"`
+	HasDraft     bool      `json:"hasDraft"`
+	Name         string    `json:"name"`
+	Text         string    `json:"text"`
+	Categories   []string  `json:"categories"`
+	Related      []string  `json:"related"`
+	PublicUrl    string    `json:"publicUrl"`
+	Popularity   float64   `json:"popularity"`
+	ViewCount    int       `json:"viewCount"`
+	CreatedBy    int       `json:"createdBy"`
+	UpdatedBy    int       `json:"updatedBy"`
+	CreatedAt    time.Time `json:"createdAt"`
+	UpdatedAt    time.Time `json:"updatedAt"`
+	PublishedAt  time.Time `json:"lastPublishedAt"`
+}
+
+// Articles is a list of articles
+// Description: https://developer.helpscout.com/docs-api/articles/list/
+// Endpoint: https://docsapi.helpscout.net/v1/categories/{id}/articles
+type Articles struct {
+	Page  int        `json:"page"`
+	Pages int        `json:"pages"`
+	Count int        `json:"count"`
+	Items []*Article `json:"items"`
+}
+
+type articlesResponse struct {
+	Articles *Articles `json:"articles"`
+}
+
+func ListArticles(categoryId string) (*Articles, error) {
+	x := articlesResponse{}
+	path := "categories/" + categoryId + "/articles"
+	err := getJSON(path, nil, &x)
+	if err != nil {
+		return nil, err
+	}
+	return x.Articles, nil
+}
